81a970cf65f7f27885c15c3b666d528a2eaaa910
Kopernikus-dev <63426249+Kopernikus1@users.noreply.github.com>
2020-07-13 22:59:14 +0200

    Fix BIP38 import crashing on empty DecKey
    Problem: The QT wallet crashes when hitting "Import Address" on the BIP38 Tool Decrypt mode when the "Decrypted address result" is empty.
    
    Cause: Without previously decrypting, key is empty and key.IsValid() is called too late.
    
    Fix: Hide pushButtonImport until a key has been decrypted, only when a key is decrypted; the "Import Address" button will become visible. Hitting "Clear" will also hide the button again until a new decryption has been done.

--- src/qt/encocoin/settings/settingsbittoolwidget.cpp
+++ src/qt/encocoin/settings/settingsbittoolwidget.cpp
@@ -73,6 +73,7 @@ SettingsBitToolWidget::SettingsBitToolWidget(EncoCoinGUI* _window, QWidget *pare
 
     ui->pushButtonImport->setText(tr("Import Address"));
     setCssProperty(ui->pushButtonImport, "btn-text-primary");
+    ui->pushButtonImport->setVisible(false);
 
     connect(ui->pushLeft, &QPushButton::clicked, [this](){onEncryptSelected(true);});
     connect(ui->pushRight,  &QPushButton::clicked, [this](){onEncryptSelected(false);});
@@ -269,6 +270,7 @@ void SettingsBitToolWidget::onClearDecrypt()
     ui->lineEditKey->clear();
     ui->lineEditDecryptResult->clear();
     ui->lineEditPassphrase->clear();
+    ui->pushButtonImport->setVisible(false);
     key = CKey();
 }
 
@@ -289,6 +291,7 @@ void SettingsBitToolWidget::onDecryptClicked()
     CPubKey pubKey = key.GetPubKey();
     CBitcoinAddress address(pubKey.GetID());
     ui->lineEditDecryptResult->setText(QString::fromStdString(address.ToString()));
+    ui->pushButtonImport->setVisible(true);
 }
 
 void SettingsBitToolWidget::importAddressFromDecKey()
