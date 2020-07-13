548b7e2d7ac30c0308278f882cb5881bf7e2e871
Kopernikus-dev <63426249+Kopernikus1@users.noreply.github.com>
2020-07-13 14:19:10 +0200

    Add more information to settings info panel
    This adds the masternode count(s) and last block hash to the new settings info panel (ported from the old RPCConsole UI Window), as well as makes the informational text select-able (good for copy/pasting information, like last block height/hash).
    
    Also, initialize static strings directly in the .ui file.

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -391,7 +391,6 @@ void RPCConsole::setClientModel(ClientModel* model)
         setNumBlocks(model->getNumBlocks());
         connect(model, &ClientModel::numBlocksChanged, this, &RPCConsole::setNumBlocks);
 
-        setMasternodeCount(model->getMasternodeCountString());
         connect(model, &ClientModel::strMasternodesChanged, this, &RPCConsole::setMasternodeCount);
 
         updateTrafficStats(model->getTotalBytesRecv(), model->getTotalBytesSent());
