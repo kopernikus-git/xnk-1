b6a41c0734b082ab33028530597bfc137d683a33
Kopernikus-dev <63426249+Kopernikus1@users.noreply.github.com>
2020-09-02 20:13:01 +0200

    [GUI] Fix invalid Destination toString encoding
    Not much to explain here, an invalid toString encoding.

--- src/base58.h
+++ src/base58.h
@@ -1,9 +1,9 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin developers
-// Copyright (c) 2017-2019 The EncoCoin developers
+// Copyright (c) 2017-2020	The PIVX developers
+// Copyright (c) 2020		The EncoCoin developers (by Kopernikus-dev)
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
 /**
  * Why base-58 instead of standard base-64 encoding?
  * - Don't want 0OIl characters that look the same in some fonts and
@@ -217,7 +217,7 @@ public:
             // Invalid address
             return "";
         }
-        return EncodeDestination(dest, isP2CS ? CChainParams::PUBKEY_ADDRESS : CChainParams::STAKING_ADDRESS);
+        return EncodeDestination(dest, isP2CS ? CChainParams::STAKING_ADDRESS : CChainParams::PUBKEY_ADDRESS);
     }
 };
 
