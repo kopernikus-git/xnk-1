98610d5d3ac22a0ac587c0c7c327b365e1e2921c
Kopernikus-dev <63426249+Kopernikus1@users.noreply.github.com>
2020-09-03 17:03:57 +0200

    [RPC] Change btc to XNK in help text
    Change btc to XNK in help text for:
    
    gettxout
    getrawtransaction
    listunspent
    decoderawtransaction
    rawdelegatestake

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -633,7 +633,7 @@ UniValue gettxout(const UniValue& params, bool fHelp)
             "{\n"
             "  \"bestblock\" : \"hash\",    (string) the block hash\n"
             "  \"confirmations\" : n,       (numeric) The number of confirmations\n"
-            "  \"value\" : x.xxx,           (numeric) The transaction value in btc\n"
+            "  \"value\" : x.xxx,           (numeric) The transaction value in XNK\n"
             "  \"scriptPubKey\" : {         (json object)\n"
             "     \"asm\" : \"code\",       (string) \n"
             "     \"hex\" : \"hex\",        (string) \n"
