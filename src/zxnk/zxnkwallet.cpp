d0dc95b07328601c8bbc56aa209ff84b13b4a2de
Kopernikus-dev <63426249+Kopernikus1@users.noreply.github.com>
2020-09-04 15:24:16 +0200

    [Wallet] AddToWallet split in AddToWallet and LoadToWallet
    Commits details:
    
    1- [No functional changes] Stop using CWalletTx::WriteToDisk, and use directly CWalletDB::WriteTx.
    
    2- Performance improvement, preventing several calls to AvailableCoins in CreateTransaction.
    
    3- [No functional changes] Split AddToWallet in AddToWallet and LoadToWallet (these two were already divided by a boolean method argument)

--- src/zxnk/zxnkwallet.cpp
+++ src/zxnk/zxnkwallet.cpp
@@ -267,7 +267,7 @@ void CzXNKWallet::SyncWithChain(bool fGenerateMintPool)
 
                     //Fill out wtx so that a transaction record can be created
                     wtx.nTimeReceived = pindex->GetBlockTime();
-                    wallet->AddToWallet(wtx, false, &walletdb);
+                    wallet->AddToWallet(wtx, &walletdb);
                     setAddedTx.insert(txHash);
                 }
 
@@ -324,7 +324,7 @@ bool CzXNKWallet::SetMintSeen(const CBigNum& bnValue, const int& nHeight, const 
 
         wtx.nTimeReceived = pindex->nTime;
         CWalletDB walletdb(wallet->strWalletFile);
-        wallet->AddToWallet(wtx, false, &walletdb);
+        wallet->AddToWallet(wtx, &walletdb);
     }
 
     // Add to zxnkTracker which also adds to database
